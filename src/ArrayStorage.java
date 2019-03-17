0f46957f19e8294e7ceb3eab3b7b9c20bc6ec65b
Alexander <belymaniac@mail.ru>
2019-03-17 18:31:21 +0300

    Создан код методов в классе ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,59 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+            size = 0;
+        }
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < size; i++) {
+           if (storage[i].uuid.equals(uuid)) {
+               resume = storage[i];
+           }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        int key = size;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                key = i;
+                break;
+            }
+        }
+        if (key < size) {
+            for (int k = key; k < size - 1; k++) {
+                storage[k]  = storage[k+1];
+                size--;
+            }
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] onlyResumes = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            onlyResumes[i] = storage[i];
+        }
+        return onlyResumes;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
