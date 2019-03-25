7c5e94f00872fdd4d42ea60623d69313f8f577a2
Alexander Zavoykin <aazavoykin@gmail.com>
2019-03-25 19:56:25 +0300

    HW2 Скорректирован код под ДЗ-1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,7 +2,7 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[4];
+    Resume[] storage = new Resume[10000];
     int size = 0;
 
     public void clear() {
@@ -13,66 +13,56 @@ public class ArrayStorage {
     }
 
     public void save(Resume resume) {
-        if (find(resume) == -1) {
+        int index = find(resume.uuid);
+        if (index != -1) {
+            System.out.println("ERROR! Resume already exists. Can`t save.");
+        } else {
             if (size < storage.length) {
                 storage[size] = resume;
                 size++;
             } else {
                 System.out.println("ERROR! Not enough free space in the storage. Can`t save.");
             }
-        } else {
-            System.out.println("ERROR! Resume already exists. Can`t save.");
         }
-    }
-
-    public int find(Resume resume) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(resume.uuid)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public int find(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid.equals(uuid)) {
-                return i;
-            }
-        }
-        return -1;
     }
 
     public void update(Resume resume) {
-        if (find(resume) == -1) {
+        int index = find(resume.uuid);
+        if (index == -1) {
             System.out.println("Resume is not found. Can`t update.");
         } else {
-            storage[find(resume)] = resume;
+            storage[index] = resume;
         }
     }
 
     public Resume get(String uuid) {
-        if (find(uuid) == -1) {
+        int index = find(uuid);
+        if (find(uuid) != -1) {
+            return storage[index];
+        } else {
             System.out.println("Resume is not found. Can`t get.");
             return null;
-        } else {
-            return storage[find(uuid)];
         }
     }
 
     public void delete(String uuid) {
-        boolean isFound = false;
+        int index = find(uuid);
+        if (index == -1) {
+            System.out.println("ERROR! Resume is not found. Can`t delete.");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+        }
+    }
+
+    private int find(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                storage[i] = storage[size - 1];
-                size--;
-                storage[size] = null;
-                isFound = true;
+                return i;
             }
         }
-        if (!isFound) {
-            System.out.println("ERROR! Resume is not found. Can`t delete.");
-        }
+        return -1;
     }
 
     public Resume[] getAll() {
