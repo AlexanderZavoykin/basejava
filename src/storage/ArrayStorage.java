a5f7daffa407769f6a607d06d45e2c963dc0fa4c
Alexander Zavoykin <aazavoykin@gmail.com>
2019-03-31 11:35:53 +0300

    HW3 "Add" and "remove" methods are added. More API static methods are used.

--- src/storage/ArrayStorage.java
+++ src/storage/ArrayStorage.java
@@ -8,33 +8,20 @@ import model.Resume;
 public class ArrayStorage extends AbstractArrayStorage {
 
     @Override
-    public void save(Resume resume) {
-        if (findIndex(resume.getUuid()) != -1) {
-            System.out.println("ERROR! Resume already exists. Can`t save.");
-        } else {
-            if (size >= STORAGE_SIZE) {
-                System.out.println("ERROR! Not enough free space in the storage. Can`t save.");
-            } else {
-                storage[size] = resume;
-                size++;
-            }
-        }
+    protected void add(Resume resume) {
+        storage[size] = resume;
+        size++;
     }
 
     @Override
-    public void delete(String uuid) {
-        int index = findIndex(uuid);
-        if (index == -1) {
-            System.out.println("ERROR! Resume is not found. Can`t delete.");
-        } else {
-            storage[index] = storage[size - 1];
-            storage[size - 1] = null;
-            size--;
-        }
+    protected void remove(String uuid) {
+        storage[getIndex(uuid)] = storage[size - 1];
+        storage[size - 1] = null;
+        size--;
     }
 
     @Override
-    protected int findIndex(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
