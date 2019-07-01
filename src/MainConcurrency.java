d1ec97f8b09bfd9b014a805a9e7106d6e4f59fec
Alexander Zavoykin <aazavoykin@gmail.com>
2019-07-01 18:52:06 +0300

    HW12 first steps

--- src/MainConcurrency.java
+++ src/MainConcurrency.java
@@ -1,53 +1,41 @@
+import java.util.concurrent.*;
+
 public class MainConcurrency {
     private static int counter;
     private static final int THREAD_NUMBER = 10000;
 
     public static void main(String[] args) {
-/*        System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
-
-        // first thread
-        Thread thread0 = new Thread() {
-            @Override
-            public void run() {
-                System.out.println(getName() + ", " + getState());
-                //throw new IllegalStateException();
-            }
-        };
-        thread0.start();
-
-        // second thread
-        new Thread(() -> System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState())).start();
-        System.out.println(thread0.getState());
-
-
-        // 10000 threads more
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threadList = new ArrayList<>(THREAD_NUMBER);
-        for (int i = 0; i < THREAD_NUMBER; i++) {
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    for (int j = 0; j < 100; j++) {
-                        mainConcurrency.inc();
-                    }
-                }
-            });
-            thread.start();
-            threadList.add(thread);
-        }
+        CountDownLatch latch = new CountDownLatch(THREAD_NUMBER);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+//        CompletionService completionService = new ExecutorCompletionService(executorService);
 
-        threadList.forEach((thread -> {
+        for (int i = 0; i < THREAD_NUMBER; i++) {
+            Future<Integer> future = executorService.submit(() -> {
+                for (int j = 0; j < 100; j++) {
+                    mainConcurrency.inc();
+                }
+                latch.countDown();
+                return 5;
+            });
+            System.out.println(future.isDone());
             try {
-                thread.join();
-            } catch (InterruptedException e) {
+                System.out.println(future.get());
+            } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
-        }));
-
+        }
+        try {
+            latch.await(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        executorService.shutdown();
         System.out.println(counter);
-*/
 
-        Person person_1 = new Person("Bob");
+
+        // homework
+        /*Person person_1 = new Person("Bob");
         Person person_2 = new Person("Joe");
 
         Thread threadOne = new Thread(() -> {
@@ -59,15 +47,13 @@ public class MainConcurrency {
         });
 
         threadOne.start();
-        threadTwo.start();
+        threadTwo.start();*/
 
     }
-
 
     private synchronized void inc() {
         counter++;
     }
-
 
     private static class Person {
         private final String name;
---
+++ src/MainStream.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainStream {
+
+    private int minValue(int[] values) {
+        return 0;
+    }
+
+    private List<Integer> oddOrEven(List<Integer> integers) {
+        return new ArrayList<>();
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
