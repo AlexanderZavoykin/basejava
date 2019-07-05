4b0d8b3310b0baae8f12de59168831cb34a76e78
Alexander Zavoykin <aazavoykin@gmail.com>
2019-07-05 22:00:32 +0300

    HW12 is almost done

--- src/MainStream.java
+++ src/MainStream.java
@@ -1,19 +1,39 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class MainStream {
 
-    private int minValue(int[] values) {
-        return 0;
+    private static int minValue(int[] values) {
+        AtomicInteger ai = new AtomicInteger(0);
+        return Arrays.stream(values).distinct()
+                .map(x -> -x)  // the trick to sort in reversed order
+                .sorted()
+                .map(x -> -x * ((int) Math.pow(10, ai.getAndIncrement())))
+                .reduce(0, Integer::sum);
     }
 
-    private List<Integer> oddOrEven(List<Integer> integers) {
-        return new ArrayList<>();
+    private static List<Integer> oddOrEven(List<Integer> integers) {
+        int sum = integers.stream()
+                .reduce(0, Integer::sum);
+        return integers.stream()
+                .filter(x -> (x + sum) % 2 != 0)
+                .collect(Collectors.toList());
     }
-
-
+    
     public static void main(String[] args) {
+        System.out.println(minValue(new int[]{1, 2, 3, 3, 2, 3}));
+        System.out.println(minValue(new int[]{9, 8}));
 
+        List<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(8);
+        list.add(10);
+        list.add(2);
+        list.add(7);
+        System.out.println(oddOrEven(list));
     }
 
 }
