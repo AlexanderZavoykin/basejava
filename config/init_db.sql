676d1ac96036075d15b08354129f4f45eb85fc30
Alexander Zavoykin <aazavoykin@gmail.com>
2019-07-09 11:55:57 +0300

    Config files are added

---
+++ config/init_db.sql
@@ -0,0 +1,24 @@
+CREATE TABLE resume
+(
+    uuid      CHAR(36) NOT NULL
+        CONSTRAINT resume_pk
+            PRIMARY KEY,
+    full_name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS contact
+(
+    id          SERIAL   NOT NULL
+        CONSTRAINT contact_pk
+            PRIMARY KEY,
+    type        TEXT     NOT NULL,
+    value       TEXT     NOT NULL,
+    resume_uuid CHAR(36) NOT NULL
+        CONSTRAINT contact_resume_uuid_fk
+            REFERENCES resume
+            ON UPDATE RESTRICT ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX resume_pk
+    ON resume (uuid);
+
