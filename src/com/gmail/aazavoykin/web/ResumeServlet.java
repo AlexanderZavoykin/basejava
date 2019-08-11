52876d9be33ed9478647cfb153137c487717736a
Alexander Zavoykin <aazavoykin@gmail.com>
2019-08-11 10:46:28 +0300

    drawTable() is modified

--- src/com/gmail/aazavoykin/web/ResumeServlet.java
+++ src/com/gmail/aazavoykin/web/ResumeServlet.java
@@ -5,28 +5,28 @@ import com.gmail.aazavoykin.exception.ResumeDoesNotExistStorageException;
 import com.gmail.aazavoykin.model.Resume;
 import com.gmail.aazavoykin.storage.SqlStorage;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 public class ResumeServlet extends HttpServlet {
-    SqlStorage sqlStorage;
+    private SqlStorage sqlStorage;
 
     @Override
-    public void init() throws ServletException {
+    public void init() {
         sqlStorage = new SqlStorage(Config.getInstance().getDbUrl(),
                 Config.getInstance().getDbUser(),
                 Config.getInstance().getDbPassword());
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
         response.setContentType("text/html; charset=UTF-8");
@@ -45,13 +45,28 @@ public class ResumeServlet extends HttpServlet {
 
     private void drawTable(HttpServletResponse response) throws IOException {
         List<Resume> list = sqlStorage.getAllSorted();
-        response.getWriter().println("<table border = 1>");
+        PrintWriter writer = response.getWriter();
+        writer.println("<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Перечень резюме</title>\n" +
+                "</head>\n" +
+                "<body>");
+
+        writer.println("<table border = 1>");
+        writer.println("<tr>\n" +
+                "<td><b>Имя</b></td>");
         for (Resume r : list) {
-            response.getWriter().println("<tr>");
-            response.getWriter().println("<td>"+r.getUuid()+"</td>");
-            response.getWriter().println("<td>"+r.getFullName()+"</td>");
-            response.getWriter().println("</tr>");
+            writer.println("<tr>");
+            writer.println("<td><a href=\"resume?uuid=" + r.getUuid() + "\">" + r.getFullName() + "</a></td>");
+            //response.getWriter().println("<td>" + r.getFullName() + "</td>");
+            writer.println("</tr>");
         }
-        response.getWriter().println("</table>");
+        writer.println("</table>");
+
+        writer.println("</body>\n" +
+                "</html>");
     }
+
 }
