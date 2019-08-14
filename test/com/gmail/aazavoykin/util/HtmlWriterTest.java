bef06538f199267ae5668c3c8c98a2f8e9debc7c
Alexander Zavoykin <aazavoykin@gmail.com>
2019-08-14 13:44:07 +0300

    Added test class for HtmlWriter

---
+++ test/com/gmail/aazavoykin/util/HtmlWriterTest.java
@@ -0,0 +1,22 @@
+package com.gmail.aazavoykin.util;
+
+import com.gmail.aazavoykin.ResumeTestData;
+import com.gmail.aazavoykin.model.AbstractSection;
+import com.gmail.aazavoykin.model.SectionType;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HtmlWriterTest {
+    private AbstractSection section;
+
+    @Before
+    public void setUp() {
+        section = ResumeTestData.RESUME_1.getSection(SectionType.EXPERIENCE);
+    }
+
+    @Test
+    public void toHtml() {
+        System.out.println(HtmlWriter.toHtml(SectionType.EXPERIENCE, section));
+    }
+
+}
