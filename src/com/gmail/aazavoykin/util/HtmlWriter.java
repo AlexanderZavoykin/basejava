5a9fc7efc17a0205eb1634a401a4913cdf312e51
Alexander Zavoykin <aazavoykin@gmail.com>
2019-08-16 17:34:05 +0300

    Added method to create HTML forms for editing each section of resume

--- src/com/gmail/aazavoykin/util/HtmlWriter.java
+++ src/com/gmail/aazavoykin/util/HtmlWriter.java
@@ -2,6 +2,8 @@ package com.gmail.aazavoykin.util;
 
 import com.gmail.aazavoykin.model.*;
 
+import java.util.List;
+
 public class HtmlWriter {
 
     public static String toHtml(SectionType sectionType, AbstractSection section) {
@@ -27,6 +29,35 @@ public class HtmlWriter {
         return result;
     }
 
+    public static String createSectionForm(SectionType sectionType, AbstractSection section) {
+        String result = "<dl><dt><h3>" + sectionType.getTitle() + ":</h3></dt><br>";
+        switch (sectionType) {
+            case PERSONAL:
+            case OBJECTIVE:
+                result += "<dd><textarea form = \"edit_form\" rows = \"4\" cols=\"150\" type=\"text\" name=\"" + sectionType.name() + "\">" +
+                        ((TextSection) section).getBody() +
+                        "</textarea>" + "</dd>";
+                break;
+            case ACHIEVEMENT:
+            case QUALIFICATION:
+                List<String> skills = ((ListSection) section).getSkills();
+                int inputNum = Math.max(skills.size(), 5);
+                result += "<dd>";
+                for (int i = 0; i < inputNum; i++) {
+                    String skill = i < skills.size() ? skills.get(i) : "";
+                    result += "<li><input type=\"text\" name=\"" + sectionType.name() + "\" value=\"" + skill +
+                            "\" size=\"150\">";
+                }
+                result += "</dd></dl>";
+                break;
+            case EXPERIENCE:
+            case EDUCATION:
+                // TODO creating block of forms for each organization
+                break;
+        }
+        return result;
+    }
+
     private static String toHtml(Organization.Period p) {
         String description = p.getDescription() == null ? "" : p.getDescription();
         return "<tr>" +
@@ -38,11 +69,11 @@ public class HtmlWriter {
 
     private static String toHtml(Organization o) {
         String result = "<table><tr>" +
-                "<td colspan=\"2\"><h3>";
+                "<td colspan=\"2\"><h4>";
         String title = o.getLink().getUrl() == null ? o.getLink().getName() :
                 "<a href=\"" + o.getLink().getUrl() + "\">" + o.getLink().getName() + "</a>";
         result += title;
-        result += "</h3></td></tr>";
+        result += "</h4></td></tr>";
         for (Organization.Period p : o.getPeriods()) {
             result += toHtml(p);
         }
