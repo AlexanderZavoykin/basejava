a4251d3ed869e82e917c8b96bce537dad480173e
Alexander Zavoykin <aazavoykin@gmail.com>
2019-08-28 17:17:29 +0300

    Added final static variable HTML_FORMATTER; changed static final variable NOW; added static method toDate to get YearMonth object from String

--- src/com/gmail/aazavoykin/util/DateUtil.java
+++ src/com/gmail/aazavoykin/util/DateUtil.java
@@ -1,8 +1,19 @@
 package com.gmail.aazavoykin.util;
 
 import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class DateUtil {
-    public final static YearMonth NOW = YearMonth.of(3000, 1);
+    private final static YearMonth NOW = YearMonth.now();
+    public static final DateTimeFormatter HTML_FORMATTER = DateTimeFormatter.ofPattern("MM-uuuu");
+
+    public static YearMonth toDate(String date) {
+        try {
+            return YearMonth.parse(date, HTML_FORMATTER);
+        } catch (DateTimeParseException e) {
+            return NOW;
+        }
+    }
 
 }
